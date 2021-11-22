c3038849c583b094961aa632527ec04a0859cb3e
Rodion Sibov <rodionsibov@gmail.com>
2021-11-22 11:01:58 +0200

    update

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function typeWriter() {
 const sectionEl = document.querySelector('section')
 sectionEl.addEventListener('click', () => {
 	let random = Math.floor(Math.random() * 100) + 104
+	
 	console.log(random);
-	console.log(navigator);
-	Array.from(sectionEl).forEach(item => windows.navigator.vibrate(random))
+	Array.from(sectionEl).forEach(item => console.log(item))
 })
