c788011e1002de0c90f2cb9868ce423ea08250f4
Rodion Sibov <rodionsibov@gmail.com>
2021-11-22 13:34:09 +0200

    update

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-//type effect
 let j = 0;
 const txt = `Say Hello!`;
 const speed = 200;
 
 function typeWriter() {
 	if (j < txt.length) {
-		document.getElementById("demo").innerHTML += txt.charAt(j);
+		document.querySelector('#demo').innerHTML += txt.charAt(j);
 		j++;
 		setTimeout(typeWriter, speed);
 	}
 }
+setTimeout(typeWriter, 2000)
