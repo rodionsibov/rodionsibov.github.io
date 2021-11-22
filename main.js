92f2d93b75ec7e53f9d8561729e8cc28030f7e22
Rodion Sibov <rodionsibov@gmail.com>
2021-11-22 10:05:50 +0200

    update

--- main.js
+++ main.js
@@ -58,3 +58,54 @@ setInterval(() => {
     const emoji = currentEmoji()
     changeFavicon(emoji)
 }, delay)
+
+
+	//change filter 
+	console.log('Press key "C" to change the color')
+
+	document.onkeydown = () => {
+		if (event.keyCode == 67) {
+			//	      document.querySelector('body').classList.toggle('filter')
+			document.querySelector('body').style.background = `aliceblue`;
+			document.querySelector('body').style.color = "#333";
+			document.querySelectorAll(".contact li").forEach(el => el.style.filter = "none");
+		}
+	}
+
+
+	//type effect
+	let j = 0;
+	const txt = `Say Hello!`;
+	const speed = 200;
+
+	function typeWriter() {
+		if (j < txt.length) {
+			document.getElementById("demo").innerHTML += txt.charAt(j);
+			j++;
+			setTimeout(typeWriter, speed);
+		}
+	}
+
+
+	setTimeout(typeWriter, 3000)
+
+
+
+	function vibrate(ms) {
+		navigator.vibrate(ms);
+	}
+
+
+
+
+	const changeFavicon = emoji => {
+		// Ensure we have access to the document, i.e. we are in the browser.
+		if (typeof window === 'undefined') return;
+		const link = window.document.querySelector("link[rel*='icon']") || window.document.createElement("link");
+		link.type = "image/svg+xml";
+		link.rel = "shortcut icon";
+		link.href = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2280%22>${emoji}</text></svg>`;
+		window.document.getElementsByTagName("head")[0].appendChild(link);
+	}; // Thanks to https://formito.com/tools/favicon
+
+	changeFavicon("🧔");
